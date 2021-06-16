fac1cac00630e51fdb018aa5475309d776347fe3
S K <84536129+shreyk04@users.noreply.github.com>
2021-06-16 10:17:22 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const inputs=document.querySelectorAll(".container input");
+
+function update(){ 
+    const suffix=this.dataset.sizing ||"";
+    document.documentElement.style.setProperty(`--${this.name}` , this.value+suffix );
+   
+}
+inputs.forEach(input=>input.addEventListener('change',update));
+inputs.forEach(input=>input.addEventListener('mousemove', update));
